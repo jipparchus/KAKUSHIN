f434faeb158ae062d2761b5416723c4f8e82b116
jipparchus <mususu@kpd.biglobe.ne.jp>
2025-06-14 17:54:00 +0100

    Cleaned the code

--- backend/db/init_rdb.py
+++ backend/db/init_rdb.py
@@ -1,7 +1,7 @@
 """
 Create a SQLite database based on /models.py
 """
-
+import os
 from sqlalchemy import create_engine
 
 from config import config
@@ -11,12 +11,16 @@ from db.models import Base
 
 
 def main():
-    engine = create_engine(
-        config['database']['uri'],
-        echo=True
-    )
-    # CREATE TABLES
-    Base.metadata.create_all(engine)
+    if os.path.exists(config['paths']['database']):
+        return 0
+    else:
+        engine = create_engine(
+            config['database']['uri'],
+            echo=True
+        )
+        # CREATE TABLES
+        Base.metadata.create_all(engine)
+        return 0
 
 
 if __name__ == '__main__':
