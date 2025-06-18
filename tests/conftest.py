516e9458af4dadef61d24659f219db48d6f6a5d4
jipparchus <mususu@kpd.biglobe.ne.jp>
2025-06-18 16:38:34 +0100

    Resolving importing module not found error

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,23 @@
 import pytest
 import os
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
 # from backend.main import app
-from backend.routes import auth
 from backend.db.session import get_db
 from backend.db.models import Base
 
 from test_config import test_config
+
+# """
+# Mocks
+# """
+# @pytest.fixture(autouse=True)
+# def mock_auth(mocker):
+#     mock = mocker.patch('backend.routes.auth.get')
 
 
 if os.path.exists(test_config['paths']['database']):
@@ -33,7 +39,8 @@ Base.metadata.create_all(bind=engine)
 
 # Create FastAPI instance
 app = FastAPI()
-app.include_router(auth.router, prefix="/auth")
+# app.include_router(auth.router, prefix="/auth")
+app.include_router(APIRouter(), prefix="/auth")
 
 
 """
@@ -57,11 +64,10 @@ Telling FastAPI that whenever Depends(get_db) is called, it should use override_
 app.dependency_overrides[get_db] = override_get_db
 
 
-@pytest.fixture(scope="module", autouse=True)
+@pytest.fixture(scope="module")
 def setup_db():
     Base.metadata.create_all(bind=engine)
-    yield
-    Base.metadata.drop_all(bind=engine)
+    yield Base.metadata.drop_all(bind=engine)
 
 # Pytest fixture for the client
 
