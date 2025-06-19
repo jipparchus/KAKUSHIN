4fd2aaad1307bc56082b3b732265d454578503eb
jipparchus <mususu@kpd.biglobe.ne.jp>
2025-06-19 18:52:34 +0100

    Experimentally undo commentting out the code

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,10 +1,10 @@
-# import pytest
-# from fastapi.testclient import TestClient
-# from backend.main import app
+import pytest
+from fastapi.testclient import TestClient
+from backend.main import app
 
-# client = TestClient(app)
+client = TestClient(app)
 
 
-# def test_root():
-#     response = client.get('/auth')
-#     assert response.status_code == 200
+def test_root():
+    response = client.get('/auth')
+    assert response.status_code == 200
