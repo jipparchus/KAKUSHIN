66cce3d451474ce16c2b66d0634e465c261e3994
jipparchus <mususu@kpd.biglobe.ne.jp>
2025-06-20 13:16:54 +0100

    Safe load f config file for test

--- backend/config.py
+++ backend/config.py
@@ -3,8 +3,11 @@ import os
 
 
 def load_config():
-    config_file = 'config.yaml'
-    config_path = os.path.join(os.path.dirname(__file__), config_file)
+    # Set the default path to the config file. If not defined, return the second argument.
+    # This is set to prevent the test code from reading the production config file.
+    config_path = os.getenv(
+        'CONFIG_PATH',
+        os.path.join(os.path.dirname(__file__), 'config.yaml')
+    )
     with open(config_path, 'r') as f:
         return yaml.safe_load(f)
-
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,7 +1,14 @@
+import os
+
+
 def test_load_config():
     from unittest import mock
     mock.patch.stopall()  # cancel the global mock from conftest.py
 
+    # Patch the CONFIG_PATH environment variable
+    print('CURRENT DIR: ')
+    print(os.path.join(os.path.dirname(__file__)))
+    os.environ["CONFIG_PATH"] = str(os.path.join(os.path.dirname(__file__).replace('tests', 'backend'), 'config_template.yaml'))
     from backend.config import load_config
     config = load_config()
     # Type check
