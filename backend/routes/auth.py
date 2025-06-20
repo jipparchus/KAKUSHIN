5f82b50c579c845b870fffc4a826581fcb9b9072
jipparchus <mususu@kpd.biglobe.ne.jp>
2025-06-20 11:29:53 +0100

    injected database session to the endpoint for safe session lifecycle handling

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
 from backend.auth.jwt_utils import create_token
-# from backend.auth import jwt_utils
 from backend.auth.hashing import hashing, verify_hash
+from backend.db.dependency import get_db
 from backend.db.models import User
 from backend.db.session import get_session_local
 from backend.db.schemas import AuthData
@@ -11,32 +12,30 @@ router = APIRouter()
 
 
 @router.post('/register')
-def register(data: AuthData):
+def register(data: AuthData, db: Session = Depends(get_db)):
     """
     On Sign up
+    injecting the database session to handle the session lifecycle safely.
     """
     hashed_pw = hashing(data.password)
-    # Check with the DB
-    rdb = get_session_local()
     # Already exist user
-    if rdb.query(User).filter_by(username=data.username).first():
+    if db.query(User).filter_by(username=data.username).first():
         raise HTTPException(status_code=400, detail='Username exists')
     user = User(username=data.username, hashed_password=hashed_pw)
-    rdb.add(user)
+    db.add(user)
     print('######################################')
     print(user.id)
-    rdb.commit()
+    db.commit()
     token = create_token({'user_id': user.id})
     return {'access_token': token}
 
 
 @router.post('/login')
-def login(data: AuthData):
+def login(data: AuthData, db: Session = Depends(get_db)):
     """
     On Sign in
     """
-    rdb = get_session_local()
-    user = rdb.query(User).filter_by(username=data.username).first()
+    user = db.query(User).filter_by(username=data.username).first()
     if not user or not verify_hash(data.password, user.hashed_password):
         raise HTTPException(status_code=401, detail='Invalid credentials')
     token = create_token({'user_id': user.id})
