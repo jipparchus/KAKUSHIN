00d201453d48af58876327f4990acaafb0e2a7cd
jipparchus <mususu@kpd.biglobe.ne.jp>
2025-06-21 00:14:19 +0100

    Fixed the database path to memory

--- tests/db/test_init_rdb.py
+++ tests/db/test_init_rdb.py
@@ -10,17 +10,17 @@ from backend.db.init_rdb import main
 
 
 @pytest.fixture
-def tmp_config(tmp_path):
+def tmp_config():
     """
     temp DB file for testing
     This is a function used for temporary patch of the load_config with various configurations
     """
-    db_path = tmp_path / 'test_database_temp.db'
+    db_path = 'test_database_temp.db'
     return {
         'paths': {
-            'database': str(db_path)},
+            'database': db_path},
         'database': {
-            'uri': f'sqlite:///{str(db_path)}',
+            'uri': f'sqlite:///:memory:{db_path}',
         }
     }
 
