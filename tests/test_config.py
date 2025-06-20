fa39b10330cbad83153ab3c5d214be53e49640f6
jipparchus <mususu@kpd.biglobe.ne.jp>
2025-06-20 14:58:11 +0100

    Added test code for backend/db/dependency

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,13 +1,10 @@
 import os
+from unittest import mock
 
 
 def test_load_config():
-    from unittest import mock
     mock.patch.stopall()  # cancel the global mock from conftest.py
-
     # Patch the CONFIG_PATH environment variable
-    print('CURRENT DIR: ')
-    print(os.path.join(os.path.dirname(__file__)))
     os.environ["CONFIG_PATH"] = str(os.path.join(os.path.dirname(__file__).replace('tests', 'backend'), 'config_template.yaml'))
     from backend.config import load_config
     config = load_config()
